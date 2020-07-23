493e72ef594653a438155cae1da7ed00c9a0faee
frohikey <frohikey@gmail.com>
2020-07-23 18:38:00 +0200

    Bootstrap removed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import helper from './js/helper'
 import alert from './components/alert.vue'
-import bootstrap from './js/tutu'
+//import bootstrap from './js/tutu'
 
 export default
 {
     helper,
     alert,
-    bootstrap
+    //bootstrap
 }
