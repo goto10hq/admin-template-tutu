0edab4368c672a6c36d4d36f04052a783f3c0d66
frohikey <frohikey@gmail.com>
2020-07-27 16:16:45 +0200

    Breadcrumb sample

--- app.src/app/generic.vue
+++ app.src/app/generic.vue
@@ -1,13 +1,83 @@
 ﻿<template>
-  <div>
-    <h2>{{ headline }}</h2>
-  </div>
+  <b-container fluid>
+    <!-- <h1>
+      <i class="ti-user"></i>
+      {{ headline }}
+    </h1> -->
+    <b-breadcrumb :items="breadcrumb"></b-breadcrumb>
+    <b-row>
+      <b-col md="6">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Iq</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="i in items" :key="i.name">
+              <td>{{ i.name }}</td>
+              <td>{{ i.cuteness }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </b-col>
+      <b-col md="6">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Rank</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(i, rank) in items" :key="i.name">
+              <td>{{ i.name }}</td>
+              <td>{{ rank + 1 }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 <script>
+  import Vue from 'vue'
+
   import config from '../core/config'
 
+  import BootstrapVue from 'bootstrap-vue'
+  Vue.use(BootstrapVue)
+
   export default {
+    data () {
+      return {
+        items: [
+          { name: 'Kanna', cuteness: 90 },
+          { name: 'Nenecchi', cuteness: 80 },
+          { name: 'Kumiko', cuteness: 70 }
+        ]
+      }
+    },
     computed: {
+      breadcrumb () {
+        const items = [
+          {
+            text: 'Home',
+            href: 'https://google.com'
+          },
+          {
+            text: 'Posts',
+            to: { name: 'home' }
+          },
+          {
+            text: 'Another Story',
+            active: true
+          }
+        ]
+
+        return items
+      },
       headline () {
         let name = this.$route.name
         for (let m of config.menu) {
@@ -20,7 +90,7 @@
                 return m2.text
               }
             }
-          }          
+          }
         }
 
         return '?'
--- app.src/core/_bootstrap.js
+++ app.src/core/_bootstrap.js
@@ -1,4 +1,5 @@
 ﻿import '../../src/scss/tutu.scss'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { Tutu } from '../../src/js/tutu'
 import Config from './config'
 import CustomSignIn from '../app/custom-sign-in.vue'
